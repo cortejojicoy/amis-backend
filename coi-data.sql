cb3620a95aa94e04fe1ddd26e19e2d23513f25e6
Marc Saulo <mcsaulo@up.edu.ph>
2022-07-18 15:20:44 +0800

    db: add coi and coitxn temp data

---
+++ coi-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "cois" ("coi_id", "class_id", "status", "student_id", "comment", "submitted_to_sais", "created_at", "updated_at") VALUES
+('COI-22062002RG',	1002,	'Requested',	'201501544',	'',	NULL,	'2022-07-14 04:39:54',	'2022-07-14 04:39:54'),
+('COI-220620AR6B',	1005,	'Requested',	'201501544',	'',	NULL,	'2022-07-14 04:40:31',	'2022-07-14 04:40:31'),
+('COI-22062785ZJ',	1004,	'Requested',	'201501544',	'',	NULL,	'2022-07-14 04:56:58',	'2022-07-14 04:56:58');
---
+++ coitxn-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "coitxns" ("coi_txn_id", "coi_id", "action", "committed_by", "note", "created_at", "updated_at") VALUES
+(1,	'COI-22062002RG',	'Requested',	10066367,	'Pls',	'2022-07-14 04:04:05',	'2022-07-14 04:04:05'),
+(2,	'COI-220620AR6B',	'Requested',	10066367,	'2nd time',	'2022-07-14 04:04:46',	'2022-07-14 04:04:46'),
+(3,	'COI-22062785ZJ',	'Requested',	10066367,	'Pls work',	'2022-07-14 04:05:12',	'2022-07-14 04:05:12');
