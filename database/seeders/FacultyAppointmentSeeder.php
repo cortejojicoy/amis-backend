f03f7c6ba28fde069283cafe459bf6cbb82c8236
jicoy <jicoy0120@LAPTOP-PD686PLI.localdomain>
2023-01-29 22:43:43 +0800

    chore: add database seeder for test

---
+++ database/seeders/FacultyAppointmentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FacultyAppointmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $path = base_path('database/seeders/sql/faculty-appointment-data.sql');
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Faculty Appointment data seeded!');
+    }
+}
---
+++ database/seeders/ModelTagSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ModelTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $path = base_path('database/seeders/sql/model-tag-data.sql');
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Model tag data seeded!');
+    }
+}
